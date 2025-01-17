7920689b1b42ac0b6efabf910ce544df56653cd4
Janardhan-Waghchaure2 <janardanwaghchaure3@gmail.com>
2025-01-17 11:27:33 +0530

    Create ProcessManagement.sh

---
+++ ProcessManagement.sh
@@ -0,0 +1,9 @@
+ps: Report a snapshot of current processes.
+
+kill: Terminate a process by PID.
+
+killall: Terminate processes by name.
+
+bg: Place jobs in the background.
+
+fg: Bring jobs to the foreground.
